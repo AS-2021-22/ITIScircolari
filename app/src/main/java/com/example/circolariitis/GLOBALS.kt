4dcd6ec4f431cf3f678e5ea2fa0dca4317b830db
HeyJOe <jocarmi03@gmail.com>
2021-11-19 09:47:10 +0100

    changed app icon

--- app/src/main/java/com/example/circolariitis/GLOBALS.kt
+++ app/src/main/java/com/example/circolariitis/GLOBALS.kt
@@ -2,9 +2,9 @@ package com.example.circolariitis
 
 class GLOBALS {
     companion object{ // static public
-        val PROTOCOL : String = "http://"
-        val SERVER_ADDRESS : String = "192.168.5.77"
-        val PORT : String = ":5000"
+        val PROTOCOL : String = "https://"
+        val SERVER_ADDRESS : String = "circolariitis.herokuapp.com"
+        val PORT : String = ""
         val POST_CIRCOLARI : String = PROTOCOL + SERVER_ADDRESS + PORT + "/circolari"
         val POST_FILTRI : String = PROTOCOL + SERVER_ADDRESS + PORT + "/filters"
         val GET_CIRCOLARE : String = PROTOCOL + SERVER_ADDRESS + PORT + "/circolari/"
