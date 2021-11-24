a57160f246ea1c28bb162792f58163ff4963a0b7
HeyJOe <jocarmi03@gmail.com>
2021-11-24 10:24:00 +0200

    compiled server

--- server/dist/index.js
+++ server/dist/index.js
@@ -27,10 +27,15 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv = __importStar(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
 const circolare_1 = __importDefault(require("./schema/circolare"));
 dotenv.config();
 const app = (0, express_1.default)();
+const server = http_1.default.createServer(app);
+const io = new socket_io_1.Server(server);
 const PORT = process.env.PORT || 3000;
+console.log('connectiong to DB.....');
 const filters = ['quinte', 'quarte', 'terze', 'seconde', 'prime', 'biennio', 'triennio', 'studenti', 'professori', 'ATA', 'genitori'];
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use(body_parser_1.default.json());
@@ -38,13 +43,22 @@ app.use((0, cors_1.default)());
 mongoose_1.default.connect(process.env.DB_URL || 'error')
     .then(() => {
     console.log(`DB connected`);
-    app.listen(PORT, () => console.log(`>Server listening on port ${PORT}`));
+    server.listen(PORT, () => console.log(`>Server listening on port ${PORT}`));
     //let newCircolare = new CircolareModel({id:2,title:"second",description:"bgfdbdf",tags:["quinte","seconde"]})
     //newCircolare.save().catch(e => console.log("impossible duplicate keys"))
 }).catch((e) => {
+    console.log(e);
     console.log(`DB not connected`);
 });
+io.on('connection', (socket) => {
+    //console.log('user has connected')
+    socket.on('disconnect', () => {
+        //console.log(`Socket ${socket.id} has disconnected`)
+    });
+});
 app.get('/', (req, res) => {
+    //io.emit('update',{id:0,title:"Circolare test",tags:["quinte","tutti"]})
+    io.emit('update', { id: 203, title: "this is a random title" });
     res.send('hello i\'m the server');
 });
 app.get('/circolari', (req, res) => {
@@ -52,19 +66,27 @@ app.get('/circolari', (req, res) => {
 });
 app.get('/circolari/:id', (req, res) => {
     let n = parseInt(req.params.id);
-    //await CircolareModel.findOneAndDelete({id:n})
     circolare_1.default.find({ "id": n }).then(result => res.json(result[0])).catch(e => res.json([]));
 });
 app.post('/circolari/write', async (req, res) => {
+    //console.log(req.body)
     if (req.body.password === process.env.PW_WRITE) {
-        const { titolo, descrizione, tags } = req.body;
-        const n = await circolare_1.default.countDocuments();
-        const newCircolare = new circolare_1.default({ id: n + 1, title: titolo, description: descrizione, tags: tags });
-        newCircolare.save().catch(err => console.log('element already exists'));
-        res.status(200).send('inviata');
+        const { id, titolo, descrizione, tags } = req.body;
+        if (!id || !titolo || !descrizione || !tags)
+            res.status(500).json('missing fields');
+        const newCircolare = new circolare_1.default({ id, title: titolo, description: descrizione, tags });
+        newCircolare.save()
+            .then(() => {
+            io.emit('update', { id, title: titolo, tags });
+            res.status(200).json('inviata');
+        })
+            .catch(err => {
+            console.log('element already exists');
+            res.status(500).json('db error');
+        });
     }
     else
-        res.status(500).send('wrong password');
+        res.status(500).json('wrong password');
 });
 app.post('/circolari', (req, res) => {
     if (typeof (req.body.filter) === 'string') {
